56752a301e7b965bb0d937e85e4af89ed5b3a8d7
Namsojeong <nsj050320@gmail.com>
2023-05-17 11:45:54 +0900

    23-05-17 ML_Agent 수업 세팅

---
+++ MLAgent/Assets/BallOnFloor/Scripts/FloorAgent.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class FloorAgent : Agent
+{
+    public override void Initialize()
+    {
+    }
+
+    public override void OnEpisodeBegin()
+    {
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+    }
+}
